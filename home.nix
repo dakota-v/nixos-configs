54db0e0ef121f5b0b1d297729741967d5a5445b1
Dakota Vincent <dakota@sentinelshadow.com>
2024-10-23 19:23:57 -0500

    Starship re-enabled

--- home.nix
+++ home.nix
@@ -109,17 +109,17 @@
     userEmail = "dakota@sentinelshadow.com";
   };
 
-  # starship - an customizable prompt for any shell
-#  programs.starship = {
-#    enable = true;
-#    # custom settings
-#    settings = {
-#      add_newline = false;
-#      aws.disabled = true;
-#      gcloud.disabled = true;
-#      line_break.disabled = true;
-#    };
-#  };
+   starship - an customizable prompt for any shell
+  programs.starship = {
+    enable = true;
+    # custom settings
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
 
   # alacritty - a cross-platform, GPU-accelerated terminal emulator
   programs.alacritty = {
@@ -140,9 +140,9 @@
     enable = true;
     enableCompletion = true;
     # TODO add your custom bashrc here
-#    bashrcExtra = ''
-#      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-#    '';
+    bashrcExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+    '';
 
     # set some aliases, feel free to add more or remove some
     shellAliases = {
