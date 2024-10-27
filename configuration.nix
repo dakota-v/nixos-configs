14f2246df26adab4f6edc9eedb162fe4649de60a
Dakota Vincent <dakota@sentinelshadow.com>
2024-10-27 13:14:38 -0500

    Soft Commit'\

--- configuration.nix
+++ configuration.nix
@@ -170,5 +170,8 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
   virtualisation.docker.enable = true;
+  virtualisation.docker.autoPrune.enable = true;
+  virtualisation.docker.rootless.enable = true;
+  virtualisation.docker.enableOnBoot = true;
 
 }
