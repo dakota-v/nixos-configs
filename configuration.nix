ad6f7ee670ff59700ccf202388bc4cc58fca0a87
Dakota Vincent <dakota@sentinelshadow.com>
2024-11-06 05:47:05 -0600

    Added gnome remote deaktop

--- configuration.nix
+++ configuration.nix
@@ -136,7 +136,16 @@
     gh
     vscode-fhs
   ];
+  services.xrdp.enable = true;
+  services.xrdp.defaultWindowManager = "${pkgs.gnome.gnome-session}/bin/gnome-session";
+  services.xrdp.openFirewall = true;
 
+  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
+  # If no user is logged in, the machine will power down after 20 minutes.
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
